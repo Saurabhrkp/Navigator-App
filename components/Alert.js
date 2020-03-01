cdafa208ec029dffe18aea8ee7a65b14f747bd64
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-03-01 19:51:07 +0530

    Added components

---
+++ components/Alert.js
@@ -0,0 +1,12 @@
+// packages
+import { Alert, BackHandler } from 'react-native';
+const exitAlert = () => {
+  Alert.alert(
+    'Confirm exit',
+    'Do you want to quit the app?'[
+      ({ text: 'CANCEL', style: 'cancel' },
+      { text: 'OK', onPress: () => BackHandler.exitApp() })
+    ]
+  );
+};
+export { exitAlert };
